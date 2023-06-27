61b039fa87ef2d24f2f44a4c66f1be912fe94c1b
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-27 15:54:51 +0900

    빠진 파일 추가

--- lib/di/di_setup.config.dart
+++ lib/di/di_setup.config.dart
@@ -12,15 +12,24 @@
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:mask_info_clean_arch/data/mock_datas.dart' as _i4;
+import 'package:mask_info_clean_arch/data/permission/location_permission_handler_impl.dart'
+    as _i5;
+import 'package:mask_info_clean_arch/data/repository/location_repository_impl.dart'
+    as _i7;
+import 'package:mask_info_clean_arch/data/repository/store_repository_impl.dart'
+    as _i9;
 import 'package:mask_info_clean_arch/domain/permission/location_permission_handler.dart'
     as _i3;
 import 'package:mask_info_clean_arch/domain/repository/location_repository.dart'
-    as _i5;
-import 'package:mask_info_clean_arch/domain/repository/store_repository.dart'
     as _i6;
+import 'package:mask_info_clean_arch/domain/repository/store_repository.dart'
+    as _i8;
 import 'package:mask_info_clean_arch/domain/usecase/get_near_by_stores_use_case.dart'
-    as _i7;
-import 'package:mask_info_clean_arch/presentation/main_view_model.dart' as _i8;
+    as _i10;
+import 'package:mask_info_clean_arch/presentation/main_view_model.dart' as _i11;
+
+const String _dev = 'dev';
+const String _prod = 'prod';
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -34,16 +43,36 @@ extension GetItInjectableX on _i1.GetIt {
       environmentFilter,
     );
     gh.singleton<_i3.LocationPermissionHandler>(
-        _i4.MockLocationPermissionHandler());
-    gh.singleton<_i5.LocationRepository>(_i4.MockLocationRepositoryImpl());
-    gh.singleton<_i6.StoreRepository>(_i4.MockStoreRepositoryImpl());
-    gh.singleton<_i7.GetNearByStoresUseCase>(_i7.GetNearByStoresUseCase(
-      storeRepository: gh<_i6.StoreRepository>(),
-      locationRepository: gh<_i5.LocationRepository>(),
+      _i4.MockLocationPermissionHandler(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i3.LocationPermissionHandler>(
+      _i5.LocationPermissionHandlerImpl(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i6.LocationRepository>(
+      _i4.MockLocationRepositoryImpl(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i6.LocationRepository>(
+      _i7.LocationRepositoryImpl(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i8.StoreRepository>(
+      _i4.MockStoreRepositoryImpl(),
+      registerFor: {_dev},
+    );
+    gh.singleton<_i8.StoreRepository>(
+      _i9.StoreRepositoryImpl(),
+      registerFor: {_prod},
+    );
+    gh.singleton<_i10.GetNearByStoresUseCase>(_i10.GetNearByStoresUseCase(
+      storeRepository: gh<_i8.StoreRepository>(),
+      locationRepository: gh<_i6.LocationRepository>(),
       locationPermissionHandler: gh<_i3.LocationPermissionHandler>(),
     ));
-    gh.factory<_i8.MainViewModel>(
-        () => _i8.MainViewModel(gh<_i7.GetNearByStoresUseCase>()));
+    gh.factory<_i11.MainViewModel>(
+        () => _i11.MainViewModel(gh<_i10.GetNearByStoresUseCase>()));
     return this;
   }
 }
