7fe60708b0fda6b0a881eda9c284deaf34f4dcea
Junsuk Oh <junsuk.oh@survivalcoding.com>
2023-06-27 12:16:05 +0900

    ViewModel 작성

---
+++ lib/presentation/main_view_model.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:mask_info_clean_arch/domain/usecase/get_near_by_stores_use_case.dart';
+import 'package:mask_info_clean_arch/presentation/main_state.dart';
+
+class MainViewModel with ChangeNotifier {
+  final GetNearByStoresUseCase _getNearByStoresUseCase;
+
+  MainState _state = const MainState();
+
+  MainState get state => _state;
+
+  MainViewModel(this._getNearByStoresUseCase);
+
+  Future<void> getStores() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    _state = state.copyWith(
+      stores: await _getNearByStoresUseCase.execute(),
+      isLoading: false,
+    );
+    notifyListeners();
+  }
+}
